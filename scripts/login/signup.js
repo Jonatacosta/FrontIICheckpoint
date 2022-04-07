3bc96158c994ea4b6b6efc41551af90ee4ed743f
Jonata santos <89052236+Jonatacosta@users.noreply.github.com>
2022-04-06 22:37:28 -0300

    Add files via upload

---
+++ scripts/login/signup.js
@@ -0,0 +1,14 @@
+let camponome = document.getElementById('inputnomeuser');
+let campoapelido = document.getElementById('inputapelidouser');
+let campoEmail = document.getElementById('inputemailuser');
+let camposenha = document.getElementById('inputsenhauser');
+let camporepetirsenha = document.getElementById('inputsenhauserrep');
+let botaocriar = document.getElementById('botaocriar');
+
+
+
+    botaocriar.addEventListener('click', function (evento) {
+        
+        evento.preventDefault();
+        
+    })
